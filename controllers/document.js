46d9a7bd6274feaf3f6fd895a9bed0895eb2d323
kronoid <syukron.fajri97@gmail.com>
2022-08-02 14:18:04 +0700

    update excel sale

--- controllers/document.js
+++ controllers/document.js
@@ -160,7 +160,7 @@ module.exports = {
         //table header
         worksheet.getCell('A6').value = `Item Name`;
         worksheet.getCell('B6').value = `Weight`;
-        worksheet.getCell('D6').value = `Arrival Date`;
+        worksheet.getCell('D6').value = `Date`;
 
         //table content
         for(let i in data){
@@ -187,6 +187,102 @@ module.exports = {
         });
 
         res.attachment(`Report Kiln Process.xlsx`);
+        const buffer = await workbook.xlsx.writeBuffer();
+        res.end(buffer);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Download Failed",
+        error,
+      });
+    }
+  },
+  excel_sale: async (req, res) => {
+    try {        
+        m_item.hasMany(sale, { foreignKey: "id_item" });
+        sale.belongsTo(m_item, { foreignKey: "id_item" });
+
+        const data = await sale.findAll({
+            raw: true,
+            include: [
+            {
+                model: m_item,
+                attributes: [],
+                where: { id_category: 2 },
+            },
+            ],
+            attributes: [
+            sequelize.col("sale.id"),
+            [sequelize.col(`m_item.name`), "item_name"],
+            [sequelize.col(`m_item.id`), "id_item"],
+            sequelize.col("sale.weight"),
+            [
+                sequelize.fn(
+                "DATE_FORMAT",
+                sequelize.col("sale.date"),
+                "%d-%m-%Y %H:%i"
+                ),
+                "date",
+            ],
+            ],
+        });
+
+        const workbook = new ExcelJS.Workbook();
+        const worksheet = workbook.addWorksheet('Sheet1');
+
+        worksheet.mergeCells('A1:A5');
+        worksheet.mergeCells('B1:E5');
+        worksheet.mergeCells('B6:C6');
+        worksheet.mergeCells('D6:E6');
+        for(let i = 1; i <=5; i++){            
+            worksheet.getColumn(i).width = i === 1 ? 30 : 20;
+        }
+
+        //Header
+        const logo = workbook.addImage({
+            filename:'./asset/logo_freeman.png',
+            extension: 'png'
+        });
+        worksheet.addImage(logo,{
+            tl: { col: 0, row: 1 },
+            ext: { width: 226.8, height: 42.714 }
+          });          
+        worksheet.getCell('B1').value = `PT. Freeman Indonesia
+        Email : info@freemancarbonindonesia.com | Telepon : +6221 7227413
+        Jl. Panglima Polim V No. 22 RT.01/06 Kel. Melawai Kec.Kebayoran Baru Jakarta Selatan, Indonesia
+        `;
+
+        
+        //table header
+        worksheet.getCell('A6').value = `Item Name`;
+        worksheet.getCell('B6').value = `Weight`;
+        worksheet.getCell('D6').value = `Date`;
+
+        //table content
+        for(let i in data){
+            worksheet.mergeCells(`B${Number(i)+7}:C${Number(i)+7}`);
+            worksheet.mergeCells(`D${Number(i)+7}:E${Number(i)+7}`);
+
+            worksheet.getCell(`A${Number(i)+7}`).value = data[i].item_name;
+            worksheet.getCell(`B${Number(i)+7}`).value = data[i].weight;
+            worksheet.getCell(`D${Number(i)+7}`).value = data[i].date;
+        }
+        
+        worksheet.eachRow({ includeEmpty: true }, function(row, rowNumber) {
+            row.eachCell(function(cell, cellNumber){
+                cell.font ={bold: rowNumber <= 6 ? true : false}
+                cell.style.alignment = {vertical: 'middle', horizontal: rowNumber <= 6 ? 'center':'left', wrapText:true}
+
+                cell.border = {
+                    top: {style:'thin', color: {argb:'black'}},
+                    left: {style:'thin', color: {argb:'black'}},
+                    bottom: {style:'thin', color: {argb:'black'}},
+                    right: {style:'thin', color: {argb:'black'}}
+                };
+            })
+        });
+
+        res.attachment(`Report Sale.xlsx`);
         const buffer = await workbook.xlsx.writeBuffer();
         res.end(buffer);
     } catch (error) {
